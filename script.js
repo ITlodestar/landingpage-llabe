496f13b474f49b207a703566d20ae85662e76f8b
lodestar <122153472+Ld-star0395@users.noreply.github.com>
2023-02-28 09:44:20 +0600

    add carousel

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
     let index_mobile = true;
     $(".question .under_bar, .question p").hide();
-    $(".question").click(function(e){
+    $(".question").click(function (e) {
         $(`#${this.id} .under_bar,#${this.id} p`).slideToggle();
-        let badge = "#" + this.id + " .badge"; 
-        if($(badge).html() == '-') {
+        let badge = "#" + this.id + " .badge";
+        if ($(badge).html() == '-') {
             $(badge).html('+');
         } else {
             $(badge).html('-');
         }
     });
-    $("#humger").click(function(e){ 
-        if(index_mobile) {
+    $("#humger").click(function (e) {
+        if (index_mobile) {
             $("#humger").addClass("active");
             $(".mobile-header").addClass("active");
             index_mobile = false;
@@ -21,4 +21,24 @@ $(document).ready(function(){
             index_mobile = true;
         }
     });
-  });
+    $("#carousel-1").carousel({
+        interval: 1000
+    });
+    
+    $("#carousel-1.carousel .carousel-item").each(function () {
+        const total = 3;
+        let next = $(this).next();
+    
+        for (let i = 0; i < total - 1; i++) {
+            if (!next.length) {
+                next = $(this).siblings(":first");
+            }
+            next
+                .children(":first-child")
+                .clone()
+                .appendTo($(this));
+            next = next.next();
+        }
+    });
+});
+
